128c711361c9d3706f305c0b358a81e28f59967a
Kevin Yeh <kyeh@lmkyeh060398.local>
2019-02-20 22:23:54 -0800

    Styling added for input and button

--- pages/_offerid/_username.vue
+++ pages/_offerid/_username.vue
@@ -1,44 +1,60 @@
 <template>
   <div>
-    <input v-model="input"/>
+    <input v-model="input">
     <button v-on:click="send">Send</button>
     <div>
-      <span v-for="message in messages">
-        {{ message }}
-      </span>
+      <span v-for="message in messages">{{ message }}</span>
     </div>
   </div>
 </template>
 
 <script>
-
 // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
 
 export default {
-  data: function () {
+  data: function() {
     return {
       input: "",
       messages: [],
       socket: null
-    }
+    };
   },
-  mounted: function () {
-    this.socket = new WebSocket('ws://nexpress.herokuapp.com/ws/' + this.$route.params.offerid)
-    this.$cookies.set("SOCKET", "TESTVAL")
-    this.socket.onmessage = (event) => {
-      this.messages.push(event.data)
-    }
+  mounted: function() {
+    this.socket = new WebSocket(
+      "ws://nexpress.herokuapp.com/ws/" + this.$route.params.offerid
+    );
+    this.$cookies.set("SOCKET", "TESTVAL");
+    this.socket.onmessage = event => {
+      this.messages.push(event.data);
+    };
   },
   methods: {
-    send: function () {
-      this.socket.send(this.input)
+    send: function() {
+      this.socket.send(this.input);
     }
   }
-}
+};
 </script>
 
 <style>
-  span {
-    display: block;
-  }
+span {
+  display: block;
+}
+button {
+  background-color: #008cba;
+  border: none;
+  border-radius: 12px;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+input {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
 </style>
